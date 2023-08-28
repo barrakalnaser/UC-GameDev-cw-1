a91ab11fbb2a06ba9b24d1c285c5bf8cd86ce94d
barrakalnaser <142906295+barrakalnaser@users.noreply.github.com>
2023-08-28 15:56:35 +0300

    done

---
+++ ClassWork1/Assets/day1.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class day1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string heroname = "barak";
+        int herohieght = 183;
+        int heroage = 14;
+        string herosuperpowers = "speed";
+        string villainname = "shadow";
+        int villainage = 13;
+        int villainheght = 180;
+        string villiansuperpower = "strength";
+        print(heroname);
+        print(heroage);
+        print(herohieght);
+        print(herosuperpowers);
+        print(villainname);
+        print(villiansuperpower);
+        print(villainheght);
+        print(villainage);
+        print(villainage - heroage);
+
+            
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
